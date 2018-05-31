d187f730e433af8cf8d86cf9ae85ada86d00cbb5
Romain <rd2016@ic.ac.uk>
2018-05-31 23:32:44 +0100

    fixed git link

--- ChangeXchange/scripts/tag.sh
+++ ChangeXchange/scripts/tag.sh
@@ -4,7 +4,7 @@ if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
   git config --global user.email "rdespoelberch@yahoo.com"
   git config --global user.name "romdespoel"
 
-  git remote add release "https://${GH_TOKEN}@github.com/zurfyx/travis-android.git"
+  git remote add release "https://${GH_TOKEN}@github.com/gml16/changeXchange.git"
 
   git push -d release latest
   git tag -d latest
