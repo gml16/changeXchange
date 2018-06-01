4f12f060cea6cf6f5c8194cc9f9b6afc47c79dd4
Remi  Uzel <remi.uzel16@imperial.ac.uk>
2018-06-01 09:55:57 +0100

    added accept button to offers

---
+++ ChangeXchange/app/src/main/java/entity/changexchange/sendText.java
@@ -0,0 +1,17 @@
+package entity.changexchange;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class sendText extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_text2);
+
+
+    }
+
+
+}
--- ChangeXchange/app/src/main/java/entity/changexchange/utils/OfferAdapter.java
+++ ChangeXchange/app/src/main/java/entity/changexchange/utils/OfferAdapter.java
@@ -2,16 +2,22 @@ package entity.changexchange.utils;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
 
+import entity.changexchange.MainActivity;
 import entity.changexchange.R;
+import entity.changexchange.sendText;
+
+import static android.support.v4.content.ContextCompat.startActivity;
 
 public class OfferAdapter extends RecyclerView.Adapter<OfferAdapter.OfferViewHolder> {
 
@@ -52,6 +58,13 @@ public class OfferAdapter extends RecyclerView.Adapter<OfferAdapter.OfferViewHol
                 offer.getBuying().toString(),
                 offer.getSelling().toString()
         );
+
+//        holder.accept.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(MainActivity.this, sendText.class));
+//            }
+//        });
     }
 
     @Override
@@ -63,12 +76,14 @@ public class OfferAdapter extends RecyclerView.Adapter<OfferAdapter.OfferViewHol
 
         TextView title;
         TextView exchangeRate;
+        Button accept;
 
         public OfferViewHolder(View itemView) {
             super(itemView);
 
             title = itemView.findViewById(R.id.offer_title);
             exchangeRate = itemView.findViewById(R.id.offer_rate);
+            accept = itemView.findViewById(R.id.offer_accept);
         }
     }
 
